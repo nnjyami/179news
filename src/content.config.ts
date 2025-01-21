e5ce059b4da7db704627b05ccf82bb19b17ddc94
nnjyami <y@179.jp>
2025-01-21 17:57:24 +0900

    インタビューディレクトリ追加

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
+// NEWS
 const news2025 = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/2025/", pattern: "**/*.{md,mdx}" }),
@@ -15,4 +16,18 @@ const news2025 = defineCollection({
   }),
 });
 
-export const collections = { news2025 };
+// INTERVIEWS
+const interviews = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/interviews/", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { news2025, interviews };
